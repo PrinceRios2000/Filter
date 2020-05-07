f076fbac50b927bfddb70d07a4cb328ee007d79b
PrinceRios2000 <56173707+PrinceRios2000@users.noreply.github.com>
2020-05-06 21:56:01 -0700

    Add files via upload

---
+++ filter.py
@@ -0,0 +1,25 @@
+import time
+import cv2
+from flask import Flask, render_template, Response
+from PIL import Image
+
+
+
+class Filter:
+	def find_filter(self, audio):
+		dic = {'red' : cv2.COLOR_BGR2GRAY}
+		default = 'did not find it here'
+		for key, value in dic.items():
+			if audio == key:
+				return value
+				break
+		return default
+		return 'ok'
+
+
+
+
+
+
+
+
